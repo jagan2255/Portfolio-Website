e48c85ab00fd866ca0adbe296813efa29cfd9075
jagan2255 <101348715+jagan2255@users.noreply.github.com>
2022-06-09 22:34:53 +0530

    Update reviewmodel.js

--- BackEnd/src/model/reviewmodel.js
+++ BackEnd/src/model/reviewmodel.js
@@ -2,7 +2,6 @@ const mongoose =require("mongoose");
 mongoose.connect("mongodb://localhost:27017/Testimony");
 
 const Schema = mongoose.Schema;
-
 const PeoplesSchema = new Schema({
 
     slno: Number,
